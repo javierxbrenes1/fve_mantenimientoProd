a787670ff506d4b0efe66c68a2eccde09fce6a29
Javier Brenes <javierxbrenes@outlook.es>
2016-01-26 00:09:45 -0600

    Se agrego la etiqueta al splash

--- Mantenimiento_Productos/Mantenimiento_Productos/UI/UISplash.Designer.cs
+++ Mantenimiento_Productos/Mantenimiento_Productos/UI/UISplash.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UISplash));
             this.pboxImagen = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,27 +41,40 @@
             this.pboxImagen.InitialImage = null;
             this.pboxImagen.Location = new System.Drawing.Point(0, 0);
             this.pboxImagen.Name = "pboxImagen";
-            this.pboxImagen.Size = new System.Drawing.Size(577, 207);
+            this.pboxImagen.Size = new System.Drawing.Size(615, 207);
             this.pboxImagen.TabIndex = 0;
             this.pboxImagen.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VerFrutaExpress | 2016";
             // 
             // UISplash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(577, 251);
+            this.ClientSize = new System.Drawing.Size(615, 252);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pboxImagen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "UISplash";
             this.Text = "UISplash";
             ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pboxImagen;
+        private System.Windows.Forms.Label label1;
     }
 }
