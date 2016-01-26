fc5bad9361a6f1015fc139868d418f0716ae0ec3
Javier Brenes <javierxbrenes@outlook.es>
2016-01-26 00:04:31 -0600

    26-01-2016 estructura del proyecto para el mantenimiento de materiales

---
+++ Mantenimiento_Productos/Mantenimiento_Productos/Program.cs
@@ -0,0 +1,23 @@
+﻿using Mantenimiento_Productos.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mantenimiento_Productos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UISplash());
+        }
+    }
+}
---
+++ Mantenimiento_Productos/Mantenimiento_Productos/UI/UISplash.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Mantenimiento_Productos.UI
+{
+    partial class UISplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UISplash));
+            this.pboxImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pboxImagen
+            // 
+            this.pboxImagen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pboxImagen.Image = ((System.Drawing.Image)(resources.GetObject("pboxImagen.Image")));
+            this.pboxImagen.InitialImage = null;
+            this.pboxImagen.Location = new System.Drawing.Point(0, 0);
+            this.pboxImagen.Name = "pboxImagen";
+            this.pboxImagen.Size = new System.Drawing.Size(577, 207);
+            this.pboxImagen.TabIndex = 0;
+            this.pboxImagen.TabStop = false;
+            // 
+            // UISplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(577, 251);
+            this.Controls.Add(this.pboxImagen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UISplash";
+            this.Text = "UISplash";
+            ((System.ComponentModel.ISupportInitialize)(this.pboxImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pboxImagen;
+    }
+}
